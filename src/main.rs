e949ffb16b75718a13a36dc9e933d80230951153
STNeto1 <gfreitasneto18@gmail.com>
2023-02-08 21:39:24 -0300

    get item

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use anyhow::{anyhow, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
 use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+use aws_sdk_dynamodb::output::QueryOutput;
+
+use crate::url::Url;
 
 mod url;
 
@@ -8,22 +12,53 @@ async fn main() -> Result<(), Error> {
     let config = aws_config::load_from_env().await;
     let client = Client::new(&config);
 
-    let resp = client
+    let basic_url = Url {
+        url: String::from("https://github.com/stneto1"),
+        short_url: String::from("another_short_url"),
+    };
+
+    //store_item(&basic_url, &client).await?;
+    let item = query_item(&basic_url.short_url, &client).await?;
+    println!("{:?}", item);
+
+    return Ok(());
+}
+
+async fn query_item(short_url: &String, client: &Client) -> Result<Url, Error> {
+    if let Ok(item) = client
+        .get_item()
+        .table_name("Url_Shortener")
+        .key("short_url", AttributeValue::S(short_url.to_owned()))
+        .send()
+        .await {
+        if let Some(raw_url) = item.item() {
+            if let Some(url) = Url::from_raw_dynamo(&raw_url) {
+                return Ok(url);
+            }
+
+            bail!("some error parsing");
+        } else {
+            bail!("some error");
+        }
+    }
+    bail!("failed to get item");
+}
+
+async fn store_item(payload: &Url, client: &Client) -> Result<(), Error> {
+    client
         .put_item()
         .table_name("Url_Shortener")
         .item(
             "short_url",
-            aws_sdk_dynamodb::model::AttributeValue::S(String::from("some-short-url")),
+            aws_sdk_dynamodb::model::AttributeValue::S(String::from(payload.short_url.as_str())),
         )
         .item(
             "url",
-            aws_sdk_dynamodb::model::AttributeValue::S(String::from("https://github.com/stneto1")),
+            aws_sdk_dynamodb::model::AttributeValue::S(String::from(payload.url.as_str())),
         )
         .send()
         .await
         .map_err(|e| anyhow!("{}", e))?;
-
-    println!("{:?}", resp);
 
     return Ok(());
 }
--- src/url.rs
+++ src/url.rs
@@ -1,7 +1,40 @@
+use std::collections::HashMap;
+
+use aws_sdk_dynamodb::model::AttributeValue;
+use aws_sdk_dynamodb::output::GetItemOutput;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Url {
-    url_hash: String,
-    url: String,
+    pub short_url: String,
+    pub url: String,
+}
+
+impl Url {
+    pub fn from_raw_dynamo(data: &HashMap<String, AttributeValue>) -> Option<Self> {
+        let (url, valid_url) = match data.get("url") {
+            Some(url) => (parse_type_cuz_dynamo_sucks(url), true),
+            None => (String::from(""), false)
+        };
+        let (short_url, valid_short_url) = match data.get("short_url") {
+            Some(short_url) => (parse_type_cuz_dynamo_sucks(short_url), true),
+            None => (String::from(""), false)
+        };
+
+        if !valid_url || !valid_short_url {
+            return None;
+        }
+
+        return Some(Self {
+            short_url,
+            url,
+        });
+    }
+}
+
+fn parse_type_cuz_dynamo_sucks(data: &AttributeValue) -> String {
+    return match data {
+        AttributeValue::S(s) => s.to_owned(),
+        _ => String::from("")
+    };
 }
